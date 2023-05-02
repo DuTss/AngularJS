66792f25f9913ec4812842d2385283cc76e03bde
DuTss <73641368+DuTss@users.noreply.github.com>
2023-05-03 01:08:28 +0200

    début update-user

--- front/src/app/homepage/homepage.component.ts
+++ front/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
 
+  constructor (private AuthService: AuthService) {}
+  isAuthenticated(): boolean {
+    return this.AuthService.getToken() !== null;
+  }
 }
